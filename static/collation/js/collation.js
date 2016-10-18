56903f1660c950b13624beaac9ffa02d61e9f7aa
INTF <intf@ntvmr.uni-muenster.de>
2016-10-18 13:45:14 +0200

    updated credits div to not mess up width of body

--- static/collation/js/collation.js
+++ static/collation/js/collation.js
@@ -3080,7 +3080,7 @@ var CL = (function () {
 			var logoRootURL = SITE_DOMAIN;
 			if (logoRootURL.length < 4 || logoRootURL.substring(0,4) !== 'http') logoRootURL = 'http://'+SITE_DOMAIN;
 			html += '</h1><div id="message_panel"></div>';
-			html += '<div id="credits" style="float:right;margin-left:2em;margin-right:-6px;">';
+			html += '<div id="credits" style="float:right;margin-left:2em;margin-right:-6px;position:absolute;right:0">';
 			html += '<div style="text-align:right;float:left;margin-right:0.5em;margin-top:5px;color:#f2f2f2;"><span style="font-weight:bold;">THE WORKSPACE FOR COLLABORATIVE EDITING</span><br/>Collation and Apparatus Editor by</div>';
 			html += '<img style="z-index:2;position:absolute;height:34px;margin-top:13px;margin-left:4px;"src="'+logoRootURL+'/bham-logo.svg"/>';
 			html += '<img style="height:55px;margin-top:-8px;border-radius:0.3em" src="'+logoRootURL+'/itsee-logo.png"/>';
